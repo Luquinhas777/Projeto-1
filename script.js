55e56eb54b311fa06121854df81459ccc43470ee
Luquinhas777 <130705766+Luquinhas777@users.noreply.github.com>
2023-05-30 19:15:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function
+
+function ativaLetra(elemento){
+    const arrTexto = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    arrTexto.forEach((Letra, i) =>{
+        setTimeout(()=>{
+            elemento.innerHTML += Letra;
+        }, 75 * i);
+    });
+}
+
+
+
+const titulo = document.querySelector('.digitando');
+ativaLetra(titulo);
+
+
